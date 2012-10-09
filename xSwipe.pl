186a127258c24972b267acf877ffe7a1dfadc5dd
iberianpig <yhkyky@gmail.com>
2012-10-10 02:27:07 +0900

    DE判別部分更新

--- xSwipe.pl
+++ xSwipe.pl
@@ -4,8 +4,8 @@ use strict;
 use Time::HiRes();
 use X11::GUITest qw( :ALL );
 use FindBin;
-#debug
-#use Smart::Comments;
+debug
+use Smart::Comments;
 
 my @xHist3 = ();              	# x coordinate history (3 fingers)
 my @yHist3 = ();              	# y coordinate history (3 fingers)
@@ -13,37 +13,27 @@ my @xHist4 = ();              	# x coordinate history (4 fingers)
 my @yHist4 = ();              	# y coordinate history (4 fingers)
 my @xHist5 = ();             	# x coordinate history (5 fingers)
 my @yHist5 = ();				# y coordinate history (5 fingers)
-
-
 my $axis="0";
 my $rate="0";
 my $lastTime = 0; 				# time monitor for TouchPad event reset
 my $eventTime = 0;			# ensure enough time has passed between events
 my $eventString = "default";	# the event to execute
-my $synCmd = qq{synclient TouchpadOff=1 -m 20};
-my $currWind = GetInputFocus();
-die "couldn't get input window" unless $currWind;
-open(INFILE," $synCmd |") or die "can't read from synclient";
-
-#debug#
-#my $windowName = GetWindowName($currWind);
 
 #設定ファイル読込
-
-my $command = qq{update-alternatives --display x-session-manager};
-open (fileHundle, " $command |");
-my @data = <fileHundle>;
-my $sessionName = (split " ",(split "/", $data[1])[3])[0];
-close(fileHundle);
-
-### $sessionName got:$sessionName
-
-my $script_dir = $FindBin::Bin;
-#CurrentPath
+my $script_dir = $FindBin::Bin;#CurrentPath
 my $confFileName="eventKey.cfg";
 my $conf = require $script_dir."/".$confFileName;
 
-### $conf= got:$conf
+my $command = qq{pgrep -lf ^gnome-session};
+open (fileHundle, " $command |");
+my @data = <fileHundle>;
+my $sessionName = (split "session=", $data[0])[1];
+close(fileHundle);
+chomp($sessionName);
+
+if ($sessionName eq undef){$sessionName='other'};
+### $command got:$command
+### $sessionName got:$sessionName
 
 my $swipeRight3=$conf->{$sessionName}->{finger3}->{right};
 my $swipeLeft3=$conf->{$sessionName}->{finger3}->{left};
@@ -60,24 +50,18 @@ my $swipeLeft5=$conf->{$sessionName}->{finger5}->{left};
 my $swipeDown5=$conf->{$sessionName}->{finger5}->{down};
 my $swipeUp5=$conf->{$sessionName}->{finger5}->{up};
 
+my $synCmd = qq{synclient TouchpadOff=1 -m 10};
+my $currWind = GetInputFocus();
+die "couldn't get input window" unless $currWind;
+open(INFILE," $synCmd |") or die "can't read from synclient";
+
 while( my $line  = <INFILE>){
 
-    #$currWind = GetInputFocus();
-    #$windowName=GetWindowName(GetInputFocus()-1)|| "NoWindowName";
-    ## got:$currWind
-    ## got:$windowName
-
-    #~ foreach(@WindowIds){
-        #~ if($_==$currWind-1){
-            #~ print "this Window is gedit's";
-        #~ }
-    #~ }
-
-    chomp($line);
+	chomp($line);
     my($time, $x, $y, $z, $f, $w) = split " ", $line;
     next if( $time =~ /time/ ); #ignore header lines
 
-    if( $time - $lastTime > 0.5 ){
+    if( $time - $lastTime > 0.3 ){
         @xHist3 = ();
         @yHist3 = ();
         @xHist4 = ();
@@ -87,28 +71,16 @@ while( my $line  = <INFILE>){
     }#if time reset
 
     $lastTime = $time;
-
 	$axis="0";
 	$rate="0";
-	
-	if($f==0){
-		`synclient TouchPadOff=0`;
-	}
 	if($f==3){
-		if( @xHist3 > 4 ){
-			`synclient TouchPadOff=1`;
-		}
 		cleanHist(4,5);
         push @xHist3, $x;
         push @yHist3, $y;
-        ### $axis before = got:$axis
         $axis=getAxis(\@xHist3,\@yHist3);
-        ### $axis after = got:$axis
         if($axis eq "x"){
-			### x got:$axis
 			$rate=getRate(@xHist3);
 		}elsif($axis eq "y"){
-			### y got:$axis
 			$rate=getRate(@yHist3);	
 		}
 
@@ -119,7 +91,6 @@ while( my $line  = <INFILE>){
         $axis=getAxis(\@xHist4,\@yHist4);
         if($axis eq "x"){
 			$rate=getRate(@xHist4);
-			
 		}elsif($axis eq "y"){
 			$rate=getRate(@yHist4);	
 		}
@@ -133,10 +104,9 @@ while( my $line  = <INFILE>){
 			$rate=getRate(@xHist5);
 			
 		}elsif($axis eq "y"){
-			$rate=getRate(@yHist5);	
+			$rate=getRate(@yHist5);
 		}
 	}else{
-		#clean all history;
 		cleanHist(3,4,5);	
 	}
 
@@ -147,14 +117,12 @@ while( my $line  = <INFILE>){
 	
     # only process one event per time window
     if( $eventString ne "default" ){
-        if( abs(time - $eventTime) > 0.7 ){
+        if( abs(time - $eventTime) > 0.3 ){
             $eventTime = time;
             SendKeys( "$eventString");
-            `synclient TouchPadOff=0`;
         }#if enough time has passed
         $eventString = "default";
     }#if non default event
-
 }#synclient line in
 
 close(INFILE);
@@ -164,34 +132,24 @@ sub getRate{
     my $rtn="0";
     my @srt = sort @hist;
     my @revSrt = reverse sort @hist;
-    ## @hist = got:@hist
-    ## @srt = got:@srt
-    ## @revSrt = got:@revSrt
-    
     if( "@srt" eq "@hist" ){
         $rtn = "+";
     }elsif( "@revSrt" eq "@hist" ){ 
         $rtn = "-";
     }#if forward or backward
-
-	### $rate got:$rtn	
 	return $rtn;
 }
 
 sub getAxis{
-	###getAxis start
 	my($xHist, $yHist)=@_;
 	my $rtn ="0";
 	if(@$xHist>5 or @$yHist>5){
-		if( abs( @$xHist[0] - @$xHist[4] ) > abs( @$yHist[0] - @$yHist[4] )){
-			## @$xHist[0] got:@$xHist[0]
+		if( abs( @$xHist[0] - @$xHist[5] ) > abs( @$yHist[0] - @$yHist[5] )){
 			$rtn="x";
 		}else{
-			## @$yHist[0] got:@$yHist[0]
 			$rtn="y";
 		}
 	}
-	### getAxis rtn got:$rtn
 	return $rtn;
 }
 
